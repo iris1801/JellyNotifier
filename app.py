6b9748148ddbdfa46bf8e34f096259d295370e7e
iris1801 <44752281+iris1801@users.noreply.github.com>
2025-01-02 11:33:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -130,7 +130,8 @@ def start_scheduler():
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///settings.db'
 db = SQLAlchemy(app)
 
-#Classi chiamate api
+
+# Classe unificata Service
 class Service(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     jellyfin_url = db.Column(db.String(255), nullable=False)
@@ -155,10 +156,6 @@ class SMTPSettings(db.Model):
     password = db.Column(db.String(120), nullable=False)
     from_email = db.Column(db.String(120), nullable=False)
 
-class Service(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    jellyfin_url = db.Column(db.String(255), nullable=False)
-    jellyfin_api_key = db.Column(db.String(255), nullable=False)
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
